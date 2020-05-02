ae9b843404eb8fe2de2cbf2f64de4bd86560e50b
prashantnisal2017 <prashantnisal2017@gmail.com>
2020-05-02 13:24:54 +0530

    initial comment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  state = {
+    text: ""
+  };
 
+  handleAdd=  async e =>{
+    await this.setState({
+      text: e.target.value
+    });
+  }
+
+  handleSubmit= e =>{
+    e.preventDefault();
+    console.log(this.state.text);
+    let formData = new FormData();
+    formData.append("text",this.state.text);
+    // const url= "http://localhost/vote/react-bknd/"
+    const url= "https://rotaractmun.org/vote-bknd/"
+    axios.post(url,formData).then(res=> console.log(res.data)).catch(err=> console.log(err));
+  }
+
+  render(){
+    return (
+      <div className="App-header">
+        <input 
+        className="form-control"
+        type="text" 
+        id="text"
+        placeholder="Enter text here"
+        onChange= {this.handleAdd}
+      />
+        <button 
+        className="btn btn-success"
+        id="submit"
+        onClick= {this.handleSubmit}>
+          submit
+        </button>
+
+      </div>
+    );
+  }
+}
 export default App;
